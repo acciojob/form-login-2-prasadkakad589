bd0d149102a9d42e41b2da1939c100e25c29a1bc
Prasad Kakad <prasadkakad118@gmail.com>
2026-03-19 10:09:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let fn=document.getElementById("FirstName");
+let ln=document.getElementById("LastName");
+let pn=document.getElementById("PhoneNumber");
+let mail=document.getElementById("EmailID");
+let submit=document.getElementById("submit");
+
+function sub(){
+	alert(`First Name: ${fn.value} Last Name: ${ln.value} 
+	Phone Number: ${pn.value} Email ID: ${mail.value}`);
+}
